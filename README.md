b0cf3f5b3adb2925fdc0deaf16511510063e4043
me <liuyi@wdcloud.cc>
2021-04-21 17:39:37 +0800

    initial

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# axios
-axios
+# axios 二次封装
+  相同错误处理   配合message回调使用 相同错误抛出一次
+  防抖          相同的接口一定时间里只触发第一次
+  通用方法        Request
+  get url传参     GetByUrl
+  post url传参    PostByUrl
+  get body传参    GetByBody
+  post body传参   PostByBody
 
 ## 安装
 
@@ -12,52 +18,75 @@ npm i @stroll/axios
 import axios from '@stroll/axios'
 // 声明实例并初始化参数
 const Axios = new axios({
-  * @param { baseURL: 'https://api.***.com' } String 域名 必传参数
-  * @param { url: '/getUserInfo' } String 请求路径 必传参数
-  * @param { withCredentials: true | false } Boolean 是否允许携带凭证 可选参数 默认true
-  * @param { method: 'get' | 'post' | 'put' | 'delete' | ... } String 请求方式 可选参数 默认get
-  * @param { timeout: 3000 } Number 请求超时 单位毫秒 可选参数 默认3秒
-  * @param { headers: {} } Json header体 可选参数 默认为空
-  * @param { data: {} } Json|Number|String|Array body体 可选参数 默认为空
-  * @param { params: {} } Json URL参数 可选参数 默认为空
-  * @param { reqFn: (config) => {} } 函数 请求前拦截 参数config
-  * @param { resFn: (response) => {} } 函数 响应后拦截 参数response
-  * @param { res: (res) => {} } 函数 请求成功处理 回传参数res
-  * @param { rej: (err) => {} } 函数 请求失败处理 回传参数err
-  * @param { wait: [{ method: 请求方式, url: 请求路径 }] } Array 需要同步的接口 方式为可选参数，路径为必传参数
+  baseURL: 'https://api.***.com', // String 域名 必传参数
+  url: '/getUserInfo', // String 请求路径 必传参数(初始化可不传)
+  method: 'get' | 'post' | 'put' | 'delete' | ..., // String 请求方式 可选参数 默认get
+  timeout: 3000, // Number 请求超时 单位毫秒 可选参数 默认3秒
+  isAntiShake: true, // 防抖开关
+  antiShakeTime: null, // 防抖时间，需打开isAntiShake， 默认使用timeout
+  withCredentials: true | false, // Boolean 是否允许携带凭证 可选参数 默认true
+  headers: {}, // Json header体 可选参数 默认为空
+  data: {}, // Json|Number|String|Array body体 可选参数 默认为空
+  params: {}, // Json URL参数 可选参数 默认为空
+  message: null, // 错误处理（需打开prompt）
+  reqFn: (config) => {}, // 函数 请求前拦截 参数config
+  resFn: (response) => {}, // 函数 响应后拦截 参数response
+  // 未实现
+  await: [{ method: '请求方式', url: '请求路径' } | '请求路径'], // Array[{Json}|String] 需要同步的接口 方式为可选参数，路径为必传参数
 })
-// 调用
-Axios.api({
-  * @param { baseURL: 'https://api.***.com' } String 域名 必传参数
-  * @param { url: '/getUserInfo' } String 请求路径 必传参数
-  * @param { withCredentials: true | false } Boolean 是否允许携带凭证 可选参数 默认true
-  * @param { method: 'get' | 'post' | 'put' | 'delete' | ... } String 请求方式 可选参数 默认get
-  * @param { timeout: 3000 } Number 请求超时 单位毫秒 可选参数 默认3秒
-  * @param { headers: {} } Json header体 可选参数 默认为空
-  * @param { data: {} } Json|Number|String|Array body体 可选参数 默认为空
-  * @param { params: {} } Json URL参数 可选参数 默认为空
-  * @param { reqFn: (config) => {} } 函数 请求前拦截 参数config
-  * @param { resFn: (response) => {} } 函数 响应后拦截 参数response
-  * @param { res: (res) => {} } 函数 请求成功处理 回传参数res
-  * @param { rej: (err) => {} } 函数 请求失败处理 回传参数err
-  * @param { wait: [{ method: 请求方式, url: 请求路径 }] } Array 需要同步的接口 方式为可选参数，路径为必传参数
-}).then(res => {}).catch(err => {})
-
-// 重置初始参数
-Axios.reset({
-  * @param { baseURL: 'https://api.***.com' } String 域名 必传参数
-  * @param { url: '/getUserInfo' } String 请求路径 必传参数
-  * @param { withCredentials: true | false } Boolean 是否允许携带凭证 可选参数 默认true
-  * @param { method: 'get' | 'post' | 'put' | 'delete' | ... } String 请求方式 可选参数 默认get
-  * @param { timeout: 3000 } Number 请求超时 单位毫秒 可选参数 默认3秒
-  * @param { headers: {} } Json header体 可选参数 默认为空
-  * @param { data: {} } Json|Number|String|Array body体 可选参数 默认为空
-  * @param { params: {} } Json URL参数 可选参数 默认为空
-  * @param { reqFn: (config) => {} } 函数 请求前拦截 参数config
-  * @param { resFn: (response) => {} } 函数 响应后拦截 参数response
-  * @param { res: (res) => {} } 函数 请求成功处理 回传参数res
-  * @param { rej: (err) => {} } 函数 请求失败处理 回传参数err
-  * @param { wait: [{ method: 请求方式, url: 请求路径 }] } Array 需要同步的接口 方式为可选参数，路径为必传参数
+```
+## 通用调用
+```js
+Axios.Request({
+  url: '/getUserInfo', // String 请求路径 必传参数
+  withCredentials: true | false, // Boolean 是否允许携带凭证 可选参数 默认true
+  method: 'get' | 'post' | 'put' | 'delete' | ..., // String 请求方式 可选参数 默认get
+  timeout: 3000, // Number 请求超时 单位毫秒 可选参数 默认3秒
+  headers: {}, // Json header体 可选参数 默认为空
+  data: {}, // Json|Number|String|Array body体 可选参数 默认为空
+  params: {}, // Json URL参数 可选参数 默认为空
+}).then(res => {
+  // 成功回调
+}).catch(err => {
+  // 失败回调
 })
+```
 
+## get方式URL传参
+```js
+Axios.GetByUrl(
+  url, // 请求路径
+  params, //  URL参数
+  prompt, // 是否关闭提示
+  timeout // 请求超时
+)
+```
+
+## post方式URL传参
+```js
+Axios.PostByUrl(
+  url, // 请求路径
+  params, //  URL参数
+  prompt, // 是否关闭提示
+  timeout // 请求超时
+)
+```
+## get方式body传参
+```js
+Axios.GetByBody(
+  url, // 请求路径
+  data, // body参数
+  prompt, // 是否关闭提示
+  timeout // 请求超时
+)
+```
+
+## post方式body传参
+```js
+Axios.PostByBody(
+  url, // 请求路径
+  data, // body参数
+  prompt,
+  timeout // 请求超时
+)
 ```
--- index.js
+++ index.js
@@ -1,38 +1,5 @@
 import Axios from 'axios'
-
-class NewAxios {
-  async constructor(config) {
-    this.config = config || {}
-    this.instance = await Axios.create({})
-    this.lineUp = []
-    await this.interceptors()
-  }
-
-  async reset (options) {
-    await Object.assign(this.config, options)
-    return await this.interceptors()
-  }
-
-  async interceptors () {
-    return new Promise(res => {
-      this.instance.interceptors.request.use(async config => {
-        this.config.reqFn && await this.config.reqFn(config)
-        return config
-      }, err => Promise.reject(err))
-    
-      this.instance.interceptors.response.use(async response => {
-        this.config.resFn && await this.config.resFn(response)
-        return response
-      }, err => Promise.reject(err))
-
-      res(true)
-    }).catch(err => {
-      console.log(err)
-      return err
-    })
-  }
-
-  /**
+/**
    * 
    * @param { baseURL: 'https://api.***.com' } String 域名 必传参数
    * @param { url: '/getUserInfo' } String 请求路径 必传参数
@@ -46,35 +13,242 @@ class NewAxios {
    * @param { resFn: (response) => {} } 函数 响应后拦截 参数response
    * @param { res: (res) => {} } 函数 请求成功处理 回传参数res
    * @param { rej: (err) => {} } 函数 请求失败处理 回传参数err
-   * @param { wait: [{ method: 请求方式, url: 请求路径 }] } Array 需要同步的接口 方式为可选参数，路径为必传参数
-   */
-  api (obj) {
-    obj = obj || {}
-    const { method = 'get', timeout = 3000, baseURL = '', withCredentials = false, headers = {}, data = {}, params = {}, wait = [] } = this.config
-    return new Promise((resolve, reject) => {
-      instance({
-        baseURL: obj.baseURL || baseURL,
-        withCredentials: obj.withCredentials || withCredentials,
-        method: obj.method || method,
-        url: obj.url || url,
-        timeout: obj.timeout || timeout,
-        headers: obj.headers || headers, // header体
-        data: obj.data || data, // body参数
-        params: obj.params || params //  URL参数
-      }).then(data => {
-        if (res) {
-          res(resolve, data)
-        } else {
-          resolve(data)
+   * @param { await: [{ method: 请求方式, url: 请求路径 }] } Array 需要同步的接口 方式为可选参数，路径为必传参数
+  **/
+class NewAxios {
+  constructor(config) {
+    this.config = {
+      method: 'get', // 请求方式
+      timeout: 3000, // 请求时长
+      antiShakeTime: null, // 防抖时间
+      isAntiShake: true, // 是否防抖
+      url:'', // 接口路由
+      baseURL: '', // 基础请求地址
+      prompt: true, // 是否打开报错提示（需要与message配合）
+      withCredentials: false, // 是否允许携带凭证
+      headers: {}, // header参数
+      data: {}, // body体参数
+      params: {}, // url参数
+      message: null, // 错误处理（需打开prompt）
+      reqFn: null, // 请求前拦截
+      resFn:null, // 请求后拦截
+      await: null, // 需等待的接口
+    }
+    this.lineUp = { switch: false, timeout: null, await: [], first: [], after: [] }
+    this.errTimeout = null
+    this.errorArr = []
+    this.antiShake = []
+    Object.assign(this.config, config)
+    if (this.config.await && this.config.await.length) {
+      this.config.switch = true
+      this.config.await.forEach(item => {
+        const val = JSON.stringify(item)
+        this.lineUp.await.push(val)
+      })
+    }
+
+    this.interceptors()
+  }
+
+  async reset (options) {
+    options && Object.assign(this.config, options)
+  }
+
+  async interceptors () { // 请求拦截
+    return new Promise(res => {
+      this.instance = Axios.create({})
+      this.instance.interceptors.request.use(async config => {
+        this.config.reqFn && await this.config.reqFn(config)
+        return config
+      }, err => Promise.reject(err))
+
+      this.instance.interceptors.response.use(async response => {
+        this.config.resFn && await this.config.resFn(response)
+        return response
+      }, err => {
+        if (
+          err.message.includes("timeout") ||
+          err.message.includes("Network Error")
+        ) {
+          console.error(`网络异常，请重新尝试`)
+          this.config.message && this.config.message({
+            showClose: true,
+            message: `网络异常，请重新尝试`,
+            type: 'error'
+          })
         }
+        return Promise.reject(err)
+      })
+      res(true)
+    }).catch(err => {
+      console.log(err)
+      return err
+    })
+  }
+
+  errorHandling () {
+    clearTimeout(this.errTimeout);
+    this.errTimeout = setTimeout(() => {
+      this.errorArr.forEach(item => {
+        if (item.type) {
+          item.type = 0;
+          this.config.message && this.config.message({
+            showClose: true,
+            message: item.val,
+            type: 'error'
+          })
+        }
+      });
+      this.errorArr.length = 0
+    }, 500)
+  }
+
+  getError (data, prompt) {
+    if (data.code && prompt) {
+      if (this.errorArr.length) {
+        const isExists = this.errorArr.every(
+          item => (item.val === data.message)
+        )
+        !isExists && this.errorArr.push({
+          type: 1,
+          val: data.message
+        })
+      } else {
+        this.errorArr.push({
+          type: 1,
+          val: data.message
+        })
+      }
+    }
+  }
+
+  clearAntiShake (tag) {
+    this.config.isAntiShake && setTimeout(() => {
+      delete this.antiShake[tag]
+    }, this.config.antiShakeTime || this.config.timeout)
+  }
+
+  parade (obj) {
+    const { method, url } = obj
+    const tag = JSON.stringify({ method, url })
+    if (this.lineUp.await.includes(url) || this.lineUp.await.includes(tag)) {
+      this.lineUp.first.push(this.Request(obj))
+    } else {
+      this.lineUp.after.push(obj)
+    }
+    clearTimeout(this.lineUp.timeout)
+    this.lineUp.timeout = setTimeout(() => {
+      Promise.all(this.lineUp.first).then(() => {
+        this.lineUp.after.forEach(params => {
+          this.Request(params)
+        })
+        this.lineUp.first.length = 0
+        this.lineUp.after.length = 0
       }).catch(err => {
-        if (rej) {
-          rej(reject, err)
-        } else {
-          reject(err)
-        }
+        console.log(err, {...err})
+        this.lineUp.first.length = 0
+        this.lineUp.after.length = 0
+      })
+      this.config.switch = false
+    }, 500)
+  }
+
+  Request (obj) {
+    obj = obj || {}
+    const {
+      url = this.config.url,
+      method = this.config.method,
+      timeout = this.config.timeout,
+      baseURL = this.config.baseURL,
+      prompt = this.config.prompt,
+      withCredentials = this.config.withCredentials,
+      headers = this.config.headers,
+      data = this.config.data,
+      params = this.config.params
+    } = obj
+    // if (this.lineUp.await.length && this.config.switch) {
+    //   this.parade(obj)
+    //   return
+    // }
+    const tag = JSON.stringify(obj)
+    if (this.config.isAntiShake) {
+      if (this.antiShake[tag]) {
+        return Promise.reject({
+          showClose: true,
+          code: 3003,
+          message: `重复请求: ${method} ${url}`,
+          params: obj,
+          type: 'error'
+        })
+      }
+      this.antiShake[tag] = true
+    }
+    this.errorHandling()
+    return new Promise((resolve, reject) => {
+      this.instance({
+        baseURL: baseURL,
+        withCredentials: withCredentials,
+        method: method,
+        url: url,
+        timeout: timeout,
+        headers: headers, // header体
+        data: data, // body参数
+        params: params //  URL参数
+      }).then(resData => {
+        this.getError(resData.data, prompt)
+        this.clearAntiShake(tag)
+        resolve(resData.data)
+      }).catch(err => {
+        this.clearAntiShake(tag)
+        reject(err)
         return err
       })
+    })
+  }
+
+  GetByUrl(url, params, prompt, timeout) {
+    return this.Request({
+      method: "get", // 请求方式
+      url, // 请求路径
+      timeout, // 请求超时
+      prompt,
+      params //  URL参数
+    })
+  }
+
+  PostByUrl(url, params, prompt, timeout) {
+    return this.Request({
+      method: "post", // 请求方式
+      url, // 请求路径
+      timeout, // 请求超时
+      prompt,
+      params //  URL参数
+    })
+  }
+
+  GetByBody(url, data, prompt, timeout) {
+    return this.Request({
+      method: "get", // 请求方式
+      url, // 请求路径
+      timeout, // 请求超时
+      prompt,
+      headers: {
+        "content-type": "application/json"
+      }, // header体
+      data // body参数
+    })
+  }
+
+  PostByBody(url, data, prompt, timeout) {
+    return this.Request({
+      method: "post", // 请求方式
+      url, // 请求路径
+      timeout, // 请求超时
+      prompt,
+      headers: {
+        "content-type": "application/json"
+      }, // header体
+      data // body参数
     })
   }
 }
